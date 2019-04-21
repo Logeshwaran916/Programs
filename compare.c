16a4d8337ace81203230ce4e02164d08eb368e28
Logeshwaran916 <49307397+Logeshwaran916@users.noreply.github.com>
2019-04-22 00:14:47 +0530

    Update compare.c

--- compare.c
+++ compare.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main()
 {
-  char str1[20],str2[20];
-  scanf("%s %s",&str1,&str2);
-  if(str1 > str2)
+  char s1[20],s2[20];
+  scanf("%s %s",&s1,&s2);
+  if(s1 > s2)
   {
-    printf("%s",str1);
+    printf("%s",s1);
   }
   else
   {
-    printf("%s",str2);
+    printf("%s",s2);
   }
   return 0;
 }
