734caf92d12965cbaa1f775ed8debe4fe5d457e9
Logeshwaran916 <49307397+Logeshwaran916@users.noreply.github.com>
2019-04-21 22:14:01 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+  char c;
+  if((c == 'a') || (c == 'e') || (c == 'i') || (c == 'o') || (c == 'u'))
+  {
+    printf("yes");
+  }
+  else
+  {
+    printf("No");
+  }
+  return 0;
+}
