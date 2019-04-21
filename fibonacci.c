102f62db388049339b4963c4e1be31ad81e9387d
Logeshwaran916 <49307397+Logeshwaran916@users.noreply.github.com>
2019-04-22 00:09:22 +0530

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-  int num1=1,num2=1,n,fibo;
+  int num1=1,num2=1,n,f;
   scanf("%d",&n);
   printf("%d\t %d\t",num1,num2);
   n=n-2;
   for(i=0;i<n;i++)
   {
-    fibo=num1+num2;
+    f=num1+num2;
     num1=num2;
-    num2=fibo;
-    printf("%d",fibo);
+    num2=f;
+    printf("%d",f);
   }
   return 0;
 }
