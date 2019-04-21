09493aa7d513b1094f2f3c5b3dd5cffed559f865
Logeshwaran916 <49307397+Logeshwaran916@users.noreply.github.com>
2019-04-22 00:17:18 +0530

    Update concatenate.c

--- concatenate.c
+++ concatenate.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main()
 {
-  char str1[20],str2[20];
+  char s[20],s[20];
   int i,j;
-  scanf("%s %s",str1,str2);
-  for(i=0;str1[i] != '\0';i++);
-  for(j=0;str2[j] != '\0';i++,j++)
+  scanf("%s %s",s1,s2);
+  for(i=0;s1[i] != '\0';i++);
+  for(j=0;s2[j] != '\0';i++,j++)
   {
-  	str1[i]=str2[j];
+  	s1[i]=s2[j];
   }
-  str1[i]='\0';
-  printf("%s",str1);
+  s1[i]='\0';
+  printf("%s",s1);
   return 0;
 }
