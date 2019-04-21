9c1190d24db7b6a97487ab1cdf4959599a0631bd
Logeshwaran916 <49307397+Logeshwaran916@users.noreply.github.com>
2019-04-21 23:57:33 +0530

    Update minute.c

--- minute.c
+++ minute.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-  int hour1,hour2,minute1,minute2,hour3,minute3;
-  scanf("%d %d",&hour1,&minute1);
-  scanf("%d %d",&hour2,&minute2);
-  hour3=hour1-hour2;
+  int hr1,hr2,minute1,minute2,hr3,minute3;
+  scanf("%d %d",&hr1,&minute1);
+  scanf("%d %d",&hr2,&minute2);
+  hr3=hr1-hr2;
   minute3=minute1-minute2;
-  printf("%d %d",hour3,minute3);
+  printf("%d %d",hr3,minute3);
 }
