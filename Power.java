4a0bde2896f536648c49def93d7a46ebbdda5fb4
Logeshwaran916 <49307397+Logeshwaran916@users.noreply.github.com>
2019-04-22 19:26:20 +0530

    Power

---
+++ Power.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class Power
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the base number");
+int base=sc.nextInt();
+int temp=base;
+System.out.println("enter the exponent number");
+int exp=sc.nextInt();
+for(int i=1;i<exp;i++)
+{
+temp=temp*temp;
+}
+System.out.println("Result is:"+temp);
+}
+}
