481a279d3ecffc4922731e0bf8e67b98d0cd7f02
Logeshwaran916 <49307397+Logeshwaran916@users.noreply.github.com>
2019-04-22 06:04:23 +0530

    Update k_natural.c

--- k_natural.c
+++ k_natural.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 int main()
 {
-  int n,i,sum;
+  int n,i,total;
   printf("Enter the number");
   scanf("%d",&n);
   if(n >= 1)
   {
-    sum=0;
+    total=0;
     for(i=1;i<=n;i++)
     {
-      sum=sum+i;
+      total=total+i;
     }
-    printf("%d",sum);
+    printf("%d",total);
   }
   return 0;
 }
