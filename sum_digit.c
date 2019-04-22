30f53fdde01e131b0ff591b6946a3956e7ed752e
Logeshwaran916 <49307397+Logeshwaran916@users.noreply.github.com>
2019-04-22 05:42:19 +0530

    Update sum_digit.c

--- sum_digit.c
+++ sum_digit.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-  int digit,remainder;
+  int N,rem;
   int sum=0;
-  scanf("%d",&digit);
-  while(digit)
+  scanf("%d",&N);
+  while(N)
   {
-    remainder=digit%10;
-    sum=sum+remainder;
-    digit=digit/10;
+    rem=N%10;
+    sum=sum+rem;
+    N=N/10;
   }
   printf("%d",sum);
   return 0;
