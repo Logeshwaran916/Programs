12e07f57f17d9a121a8a702a4287f7a8d12863f2
Logeshwaran916 <49307397+Logeshwaran916@users.noreply.github.com>
2019-04-22 20:57:13 +0530

    Listodd

---
+++ Listodd.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class Listodd
+{
+public static void main(String args[])
+{
+int limit=100000;
+System.out.println("printing odd numbers");
+for(int i=1;i<=limit;i++)
+{
+if((i%2)!=0)
+{
+System.out.println(+i"");
+}
+}
+}
+}
