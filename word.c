79faebf19246a78756f4b183cc0e5bbe1369a76c
Logeshwaran916 <49307397+Logeshwaran916@users.noreply.github.com>
2019-04-22 05:39:31 +0530

    Update word.c

--- word.c
+++ word.c
@@ -1,43 +1,43 @@
 #include<stdio.h>
 int main()
 {
-  int n;
-  scanf("%d",&n);
-  if((n >= 1) && (n <= 10))
+  int N;
+  scanf("%d",&N);
+  if((N >= 1) && (N <= 10))
   {
-    if(n == 1)
+    if(N == 1)
     {
       printf("One");
     }
-     else if(n == 2)
+     else if(N == 2)
     {
       printf("Two");
     }
-     else if(n == 3)
+     else if(N == 3)
     {
       printf("Three");
     }
-     else if(n == 4)
+     else if(N == 4)
     {
       printf("four");
     }
-     else if(n == 5)
+     else if(N == 5)
     {
       printf("Five");
     }
-     else if(n == 6)
+     else if(N == 6)
     {
       printf("six");
     }
-     else if(n == 7)
+     else if(N == 7)
     {
       printf("Seven");
     }
-     else if(n == 8)
+     else if(N == 8)
     {
       printf("Eight");
     }
-     else if(n == 9)
+     else if(N == 9)
     {
       printf("Nine");
     }
