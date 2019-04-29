7a72bc12d97e918715cf6c66c13dc34effd09f76
Logeshwaran916 <49307397+Logeshwaran916@users.noreply.github.com>
2019-04-29 22:05:57 +0530

    Update factors.c

--- factors.c
+++ factors.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-  int N,index;
-  scanf("%d",&N);
-  if(N<=1000)
+  int Num,index;
+  scanf("%d",&Num);
+  if(Num<=1000)
   {
     for(index=1;index<=N;index++)
     {
-      if(N % index == 0)
+      if(Num % index == 0)
       {
         printf("%d\t",index);
       }
