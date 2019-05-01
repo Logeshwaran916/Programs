1c4306569a9e805ae1aefffa0a7b6160c0525733
Logeshwaran916 <49307397+Logeshwaran916@users.noreply.github.com>
2019-05-01 20:40:22 +0530

    Update last_character.c

--- last_character.c
+++ last_character.c
@@ -2,14 +2,14 @@
 #include<string.h>
 int main()
 {
-  char str[20];
+  char input[20];
   int n,length,diff,i;
-  scanf("%s %d",&str,&n);
-  length=strlen(str);
+  scanf("%s %d",&input,&n);
+  length=strlen(input);
   diff=length-n;
   for(i=diff;i<length;i++)
   {
-    printf("%s",str[i]);
+    printf("%s",input[i]);
   }
   return 0;
   
