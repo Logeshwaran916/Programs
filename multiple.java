e5cc9c5e2918c487ec2aaad7d34966e097bb5ad7
Logeshwaran916 <49307397+Logeshwaran916@users.noreply.github.com>
2019-05-09 11:15:55 +0530

    multiple.java

---
+++ multiple.java
@@ -0,0 +1,14 @@
+import java.io.*;
+class multiple
+{
+public static void main(String args[])
+{
+System.out.println("divided by 5");
+for(int i=1;i<100;i++)
+{
+if(i%5==0)
+System.out.println(i+",");
+}
+System.out.println("\n");
+}
+}
